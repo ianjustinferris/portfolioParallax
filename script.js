37fd6c4d8b16bd454f34c34aef89d71c5db2f180
ianjustinferris <ianjustinferris@gmail.com>
2022-06-26 17:33:39 -0400

    Responsive home page

---
+++ script.js
@@ -0,0 +1,5 @@
+// var draw = SVG("create")
+// var rect = draw.rect(100, 100).attr({ fill: '#f06' })
+
+var draw = SVG().addTo('body').size(300, 300)
+var rect = draw.rect(100, 100).attr({ fill: '#f06' })
