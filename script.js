afc6d6e35348aa78bb64b84ed521ffe1419c3ad7
ianjustinferris <ianjustinferris@gmail.com>
2022-06-27 13:53:58 -0400

    Checking legibility of text for mobile

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-// var draw = SVG("create")
-// var rect = draw.rect(100, 100).attr({ fill: '#f06' })
-
-var draw = SVG().addTo('body').size(300, 300)
-var rect = draw.rect(100, 100).attr({ fill: '#f06' })
+const embla = EmblaCarousel(document.getElementById('embla'))
